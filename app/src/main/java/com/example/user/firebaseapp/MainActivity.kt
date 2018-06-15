44fb5a45542e7e27f7c357db72c5c2cabc3ce3d3
Mikhail Lysyansky <mikhail.lysyansky@jetruby.com>
2018-06-15 17:07:09 +0300

    Integrated Functions.

--- app/src/main/java/com/example/user/firebaseapp/MainActivity.kt
+++ app/src/main/java/com/example/user/firebaseapp/MainActivity.kt
@@ -1,91 +1,60 @@
 package com.example.user.firebaseapp
 
-import android.app.Activity
-import android.content.Intent
-import android.net.Uri
+
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.widget.Button
 import android.widget.Toast
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageMetadata
-import com.google.firebase.storage.StorageReference
-import java.io.File
-
-const val REQUEST_CODE = 1
-const val INTENT_TITLE = "Select image"
+import com.google.android.gms.tasks.Task
+import com.google.firebase.functions.FirebaseFunctions
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var firebaseStorage: FirebaseStorage
-
-    private var imgUri: Uri? = null
+    private lateinit var firebaseFunctions: FirebaseFunctions
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        firebaseStorage = FirebaseStorage.getInstance()
-
-        val buttonPick: Button = findViewById(R.id.btn_pick)
-        buttonPick.setOnClickListener { chooseImg() }
-
-        val buttonUpload: Button = findViewById(R.id.btn_upload)
-        buttonUpload.setOnClickListener { if (imgUri != null) uploadPhoto(imgUri!!) }
-
-        val buttonDownload: Button = findViewById(R.id.btn_download)
-        buttonDownload.setOnClickListener { downloadFile() }
+        firebaseFunctions = FirebaseFunctions.getInstance()
+        helloWorld()
+        addMessage()
     }
 
-    private fun chooseImg() {
-        val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.type = "image/*"
-        startActivityForResult(Intent.createChooser(intent, INTENT_TITLE), REQUEST_CODE)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            if (data != null && data.data != null) {
-                imgUri = data.data
+    private fun helloWorld(): Task<String> {
+        return firebaseFunctions
+            .getHttpsCallable("helloWorld")
+            .call()
+            .continueWith {
+                val result = it.result.data as Map<String, Any>
+                result["message"] as String
             }
-        }
-    }
-
-    private fun uploadPhoto(fileUri: Uri) {
-        val ref = firebaseStorage.reference.child("img/${fileUri.lastPathSegment}")
-        val metadata = StorageMetadata.Builder()
-            .setCustomMetadata("customMeta", "customMetavalue")
-            .build()
-
-        val task = ref.putFile(fileUri, metadata)
-        task.addOnProgressListener {
-            val progress = (100.0 * it.bytesTransferred) / it.totalByteCount
-            Toast.makeText(this, "Upload progress ${progress} %", Toast.LENGTH_SHORT).show()
-        }
-        task.addOnFailureListener {
-            Toast.makeText(this, "OnFailure ${it.message}", Toast.LENGTH_SHORT).show()
-        }
-        task.addOnCompleteListener {
-            Toast.makeText(this, "OnComplete", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun downloadFile() {
-        val file = File.createTempFile("img","png")
-        firebaseStorage.reference.child("img/image:38807")
-            .getFile(file)
-            .addOnSuccessListener {
-                Toast.makeText(this, "OnSuccess", Toast.LENGTH_SHORT).show()
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "OnComplete ${it.result}", Toast.LENGTH_SHORT).show()
+                    Log.d("log", "OnComplete ${it.result}")
+                }
             }
     }
 
-    private fun deleteFile() {
+    private fun addMessage(): Task<String> {
+        val data = HashMap<String, Any>()
+        data["text"] = "Hi, How are you?"
+        data["author"] = "Jack"
 
-
-
-
-
+        return firebaseFunctions
+            .getHttpsCallable("addMessage")
+            .call(data)
+            .continueWith {
+                val result = it.result.data as Map<String, Any>
+                result["message"] as String
+            }
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "OnComplete ${it.result}", Toast.LENGTH_SHORT).show()
+                    Log.d("log", "OnComplete ${it.result}")
+                }
+            }
     }
 }
+
+
